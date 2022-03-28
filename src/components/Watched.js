386ceeede5eefbb2adc46b8f50815f626da7712a
Marendsly <509972@vistacollege.nl>
2022-03-28 14:11:48 +0200

    New version

---
+++ src/components/Watched.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
+import {MovieCard} from './MovieCard';
+
+export const Watched = () => {
+  return (
+    <div className=''>
+    
+    </div>
+  );
+};
